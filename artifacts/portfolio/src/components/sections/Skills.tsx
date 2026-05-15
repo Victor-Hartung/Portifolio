211d1b9eb1a93a158f43b14f18ac2f1bec28fe34
Victor Alexandre Nogueira Hartung <victoralexhartung@gmail.com>
2026-05-15 20:34:03 -0300

    Update Skills.tsx

--- artifacts/portfolio/src/components/sections/Skills.tsx
+++ artifacts/portfolio/src/components/sections/Skills.tsx
@@ -26,7 +26,7 @@ export default function Skills() {
 
   const item = {
     hidden: { opacity: 0, y: 20 },
-    show: { opacity: 1, y: 0, transition: { duration: 0.5, type: "spring" } }
+    show: { opacity: 1, y: 0, transition: { duration: 0.5, type: "tween" } }
   };
 
   return (
